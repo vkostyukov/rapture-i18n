2d5f98b62c980b239abc1ff8452c9f18ca0c24df
Jon Pretty <jon.pretty@propensive.com>
2015-04-03 02:41:37 +0100

    Changed Scala version

---
+++ project/project.scala
@@ -0,0 +1,14 @@
+object project extends ProjectSettings {
+  def scalaVersion = "2.11.6"
+  def version = "1.2.0"
+  def name = "i18n"
+  def description = "Rapture I18N is a simple library for working with internationalized strings"
+  
+  def dependencies = Seq(
+  )
+  
+  def thirdPartyDependencies = Nil
+
+  def imports = Seq(
+  )
+}
